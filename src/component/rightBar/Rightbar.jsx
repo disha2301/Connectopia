6c19a2778dbcd7cafeb85ad633754ececc3f58d2
Disha <dishay166@gmail.com>
2023-09-15 18:13:44 +0530

    style rightbar

--- src/component/rightBar/Rightbar.jsx
+++ src/component/rightBar/Rightbar.jsx
@@ -1,7 +1,166 @@
 import "./rightbar.scss";
 
 const Rightbar = () => {
-  return <div className="rightBar">right</div>;
+  return (
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/1666779/pexels-photo-1666779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <span>Akash Garg</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>Cancel</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <span>Utkarsh</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>Cancel</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activites</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <p>
+                <span>Utkarsh</span> requested to follow you
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <p>
+                <span>Utkarsh</span> liked a picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <p>
+                <span>Utkarsh</span> followed you
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <p>
+                <span>Utkarsh</span> posted
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <div className="online" />
+
+              <span>Utkarsh</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <div className="online" />
+
+              <span>Utkarsh</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <div className="online" />
+
+              <span>Utkarsh</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <div className="online" />
+
+              <span>Utkarsh</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <div className="online" />
+
+              <span>Utkarsh</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <div className="online" />
+
+              <span>Utkarsh</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
