d1d5830a4e486c2bf0ba7a4b01f3b792e97610c5
Disha <dishay166@gmail.com>
2023-09-23 16:47:34 +0530

    add responsiveness

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,68 @@
 import "./profile.scss";
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Posts from "../../component/posts/Posts";
 
 const Profile = () => {
-  return <div>profile</div>;
+  return (
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/4947406/pexels-photo-4947406.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://media.licdn.com/dms/image/D5603AQHgzl2zw3fw8w/profile-displayphoto-shrink_800_800/0/1694108787694?e=1700697600&v=beta&t=6-SF4gGK_cThZAxw0c-F7g1q8HdNjzoEXPR7LoroxKs"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Abdul Ahad</span>
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>India</span>
+              </div>
+              <div className="item">
+                <LanguageIcon />
+                <span>ahad.world</span>
+              </div>
+            </div>
+            <button>follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlinedIcon />
+            <MoreVertIcon />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
